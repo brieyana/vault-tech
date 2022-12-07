16a2365f68b9ac08e69d22f8a4ade905eaa547a2
Brie <briehaynes9@gmail.com>
2022-12-06 23:16:51 -0600

    Updated pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import SignUpPage from './SignUpPage';
 import LoginPage from './LoginPage';
+import HomePage from './HomePage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<p>Home page placeholder</p>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="signup" element={<SignUpPage />} />
         <Route path="forgot_password" element={<p>Forgot password page</p>} />
